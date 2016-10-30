9f337210c97da7ec9d7aae5afe4045bab886c997
Amin <afozi732@gmail.com>
2016-10-30 12:47:36 -0700

    Add files via upload
    New pictures and data

---
+++ data.js
@@ -0,0 +1,472 @@
+var data = {
+	"schoolYear": "2016-2017",
+	"fallShows": [
+		{
+			"week": "0",
+			"day": "September 22, 2016",
+			"time": "9pm",
+			"location": "Peterson 108",
+			"theme": "Welcome Back!"
+		},
+		{
+			"week": "2",
+			"day": "October 7, 2016",
+			"time": "8pm",
+			"location": "Ledden Auditorium",
+			"theme": "Gotta FOOSH 'em all!"
+		},
+		{
+			"week": "5",
+			"day": "October 28, 2016",
+			"time": "8pm",
+			"location": "Ledden Auditorium",
+			"theme": "Halloween Spooktacular"
+		},
+		{
+			"week": "8",
+			"day": "TBA",
+			"time": "8pm",
+			"location": "Ledden Auditorium",
+			"theme": "TBA"
+		},
+		{
+			"week": "10",
+			"day": "TBA",
+			"time": "8pm",
+			"location": "Ledden Auditorium",
+			"theme": "Ugly Sweaters"
+		}
+	],
+
+	"winterShows": [
+		{
+			"week": "1",
+			"day": "TBA",
+			"time": "8pm",
+			"location": "Ledden Auditorium",
+			"theme": "Welcome Back!"
+		},
+		{
+			"week": "TBA",
+			"day": "TBA",
+			"time": "8pm",
+			"location": "Ledden Auditorium",
+			"theme": "IMPROVAPOCALYPSE"
+		},
+		{
+			"week": "TBA",
+			"day": "TBA",
+			"time": "8pm",
+			"location": "Ledden Auditorium",
+			"theme": "IMPROVACALYPSE NIGHT 2"
+		},
+		{
+			"week": "TBA",
+			"day": "TBA",
+			"time": "8pm",
+			"location": "Ledden Auditorium",
+			"theme": ""
+		},
+		{
+			"week": "TBA",
+			"day": "TBA",
+			"time": "8pm",
+			"location": "Ledden Auditorium",
+			"theme": ""
+		},
+		{
+			"week": "TBA",
+			"day": "TBA",
+			"time": "8pm",
+			"location": "Ledden Auditorium",
+			"theme": ""
+		}
+	],
+
+	"springShows": [
+		{
+			"week": "TBA",
+			"day": "TBA",
+			"time": "8pm",
+			"location": "Ledden Auditorium",
+			"theme": ""		
+		},
+		{
+			"week": "TBA",
+			"day": "TBA",
+			"time": "8pm",
+			"location": "Ledden Auditorium",
+			"theme": ""
+		},
+		{
+			"week": "TBA",
+			"day": "TBA",
+			"time": "8pm",
+			"location": "Ledden Auditorium",
+			"theme": ""		
+		},
+		{
+			"week": "TBA",
+			"day": "TBA",
+			"time": "8pm",
+			"location": "Ledden Auditorium",
+			"theme": "Alumni"		
+		},
+		{
+			"week": "TBA",
+			"day": "TBA",
+			"time": "8pm",
+			"location": "Ledden Auditorium",
+			"theme": "Senior Sendoff/Gala"
+		}
+	],
+
+	"workshops": [
+		{
+			"day": "",
+			"time": "7pm",
+			"location": "Tioga Hall (meet us outside)"
+		}
+	],
+
+	"allMembers": [
+		{
+			"name": "Lea Magro",
+			"year": "5th",
+			"fooshYear": "5th",
+			"college": "Muir",
+			"image": "leamagro.jpg"
+		},
+		{
+			"name": "Adam Whitman",
+			"year": "4rd",
+			"fooshYear": "4th",
+			"college": "Warren",
+			"image": "adamwhitman.jpg"
+		},
+		{
+			"name": "Maryam Sonboli",
+			"year": "4th",
+			"fooshYear": "4th",
+			"college": "Muir",
+			"image": "maryamsonboli.jpg"
+		},
+		{
+			"name": "Chris Rutten",
+			"year": "4th",
+			"fooshYear": "3rd",
+			"college": "Muir",
+			"image": "chrisrutten.jpg"
+		},
+		{
+			"name": "Jasmine Heredia",
+			"year": "4th",
+			"fooshYear": "3rd",
+			"college": "Muir",
+			"image": "jasmineheredia.jpg"
+		},
+		{
+			"name": "Ulises Huete",
+			"year": "3rd",
+			"fooshYear": "3rd",
+			"college": "Muir",
+			"image": "uliseshuete.jpg"
+		},
+		{
+			"name": "Kendra Quinlan",
+			"year": "3rd",
+			"fooshYear": "3rd",
+			"college": "Muir",
+			"image": "kendraquinlan.jpg"
+		},
+		{
+			"name": "Amin Fozi",
+			"year": "3rd",
+			"fooshYear": "3rd",
+			"college": "Warren",
+			"image": "aminfozi.jpg"
+		},
+		{
+			"name": "Rithvik Shankar",
+			"year": "3rd",
+			"fooshYear": "3rd",
+			"college": "Warren",
+			"image": "rithvikshankar.jpg"
+		},
+		{
+			"name": "Nick Checchia",
+			"year": "3rd",
+			"fooshYear": "3rd",
+			"college": "Marshall",
+			"image": "nickchecchia.jpg"
+		},
+		{
+			"name": "Brandon Chan",
+			"year": "3rd",
+			"fooshYear": "3rd",
+			"college": "ERC",
+			"image": "brandonchan.jpg"
+		},
+		{
+			"name": "Sami Khan",
+			"year": "4th",
+			"fooshYear": "2nd",
+			"college": "Marshall",
+			"image": "samikhan.jpg"
+		},
+		{
+			"name": "Mai Dinh",
+			"year": "4th",
+			"fooshYear": "2nd",
+			"college": "Warren",
+			"image": "maidinh.jpg"
+		},
+		{
+			"name": "Niels Griedel",
+			"year": "3rd",
+			"fooshYear": "2nd",
+			"college": "Revelle",
+			"image": "nielsgriedel.jpg"
+		},
+		{
+			"name": "Christian Ramirez",
+			"year": "3rd",
+			"fooshYear": "2nd",
+			"college": "Revelle",
+			"image": "christianramirez.jpg"
+		},
+		{
+			"name": "Josh Asiaban",
+			"year": "3rd",
+			"fooshYear": "2nd",
+			"college": "Muir",
+			"image": "joshasiaban.jpg"
+		},
+		{
+			"name": "Maddy Rae",
+			"year": "2nd",
+			"fooshYear": "2nd",
+			"college": "Marshall",
+			"image": "maddyrae.jpg"
+		},
+		{
+			"name": "Hailey Schneider",
+			"year": "2nd",
+			"fooshYear": "2nd",
+			"college": "Sixth",
+			"image": "haileyschneider.jpg"
+		},
+		{
+			"name": "Michael Yiu",
+			"year": "2nd",
+			"fooshYear": "2nd",
+			"college": "Sixth",
+			"image": "michaelyiu.jpg"
+		},
+		{
+			"name": "Parker Mace",
+			"year": "2nd",
+			"fooshYear": "2nd",
+			"college": "Revelle",
+			"image": "parkermace.jpg"
+		},
+		{
+			"name": "Rishabh Bhasin",
+			"year": "2nd",
+			"fooshYear": "2nd",
+			"college": "Revelle",
+			"image": "rishabhbhasin.jpg"
+		}
+	],
+	
+	"boardMembers": [
+		{
+			"name": "Chris Rutten",
+			"position": "Coach",
+			"year": "4th",
+			"college": "Muir",
+			"image": "chris.jpg"
+		},
+		{
+			"name": "Rithvik Shankar",
+			"position": "President",
+			"year": "3rd",
+			"college": "Warren",
+			"image": "rithvik.jpg"
+		},
+		{
+			"name": "Amin Fozi",
+			"position": "Exec",
+			"year": "3rd",
+			"college": "Warren",
+			"image": "amin.jpg"
+		},
+		{
+			"name": "Maddy Rae",
+			"position": "Exec",
+			"year": "2nd",
+			"college": "Marshall",
+			"image": "maddy.jpg"
+		},
+		{
+			"name": "This could be you!",
+			"position": "Freshman Exec",
+			"year": "",
+			"college": "",
+			"image": "anonymous.png"
+		}
+	],
+
+	"navLinks": [
+		{
+			"text": "Home",
+			"url": "index.html"
+		},
+		{
+			"text": "About",
+			"url": "about.html"
+		},
+		{
+			"text": "Shows",
+			"url": "shows.html"
+		},
+		{
+			"text": "Pictures",
+			"url": "pictures.html"
+		},
+		{
+			"text": "Members",
+			"url": "members.html"
+		},
+		{
+			"text": "Fundraising",
+			"url": "https://www.gofundme.com/4ec95x4k"
+		},
+		{
+			"text": "FAQ",
+			"url": "faq.html"
+		},
+		{
+			"text": "Contact",
+			"url": "contact.html"
+		}
+	],
+	
+	"socialMedia": [
+		{
+			"link": "https://www.facebook.com/FooshImprovComedy/",
+			"image": "facebook.jpg"
+		},
+		{
+			"link": "https://twitter.com/FooshImprov",
+			"image": "twitter.jpg"
+		},
+		{
+			"link": "https://www.instagram.com/fooshimprov/",
+			"image": "instagram.jpg"
+		},
+		{
+			"link": "http://fooshimprov.tumblr.com/",
+			"image": "tumblr.jpg"
+		}
+	],
+
+	"photoAlbums": [
+		{
+			"link": "https://goo.gl/photos/HrjHHyEKYZQQdWhd7",
+			"image": "pokemonshow.jpg",
+			"title": "Gotta FOOSH 'em All",
+			"date": "Fall Quarter: October 7, 2016"
+		},
+		{
+			"link": "https://goo.gl/photos/tT2a3jmmCpJpc8wbA",
+			"image": "survivor2016.jpg",
+			"title": "Survivor",
+			"date": "Spring Quarter: April 1, 2016"
+		},
+		{
+			"link": "https://goo.gl/photos/8emGkKkrMotTBa7QA",
+			"image": "week10winter2016.jpg",
+			"title": "Week 10 Show",
+			"date": "Winter Quarter: March 11, 2016"
+		},
+		{
+			"link": "https://goo.gl/photos/cgGNUT9CjN1BmRE99",
+			"image": "week7winter2016.jpg",
+			"title": "Week 7 Show",
+			"date": "Winter Quarter: February 19, 2016"
+		},
+		{
+			"link": "https://goo.gl/photos/6ms76jEEPsG36GPS8",
+			"image": "norcalvssocal2016.jpg",
+			"title": "NorCal vs. SoCal",
+			"date": "Winter Quarter: February 5, 2016"
+		},
+		{
+			"link": "https://goo.gl/photos/wHs7KkDVkzyaAhR47",
+			"image": "improvapocalypse2016.jpg",
+			"title": "Improvapocalypse",
+			"date": "Winter Quarter: January 22-23, 2016"
+		},
+		{
+			"link": "https://goo.gl/photos/3FzFvBCB7VBDcxcM6",
+			"image": "uglysweater2015.jpg",
+			"title": "Ugly Sweaters",
+			"date": "Fall Quarter: December 4, 2015"
+		},
+		{
+			"link": "https://goo.gl/photos/ELDGCUYWhnS3bbLs9",
+			"image": "starwars2015.jpg",
+			"title": "Star Wars",
+			"date": "Fall Quarter: November 20, 2015"
+		},
+		{
+			"link": "https://goo.gl/photos/pjwBq26TiMCNuBRHA",
+			"image": "halloween2015.JPG",
+			"title": "Halloween",
+			"date": "Fall Quarter: October 30, 2015"
+		},
+		{
+			"link": "https://goo.gl/photos/HxwL4phxu53PZqkG9",
+			"image": "backtothefoosher2015.jpg",
+			"title": "Back to the FOOSHER",
+			"date": "Fall Quarter: October 16, 2015"
+		},
+		{
+			"link": "https://goo.gl/photos/1Ukr39hpKNFAZWDJ8",
+			"image": "welcomeweek2015.jpg",
+			"title": "Welcome Week",
+			"date": "Fall Quarter: September 24, 2015"
+		},
+		{
+			"link": "https://goo.gl/photos/sc9svFRsneYhuKvZ7",
+			"image": "seniorsendoff2015.jpg",
+			"title": "Senior Sendoff",
+			"date": "Spring Quarter: June 5, 2015"
+		},
+		{
+			"link": "https://goo.gl/photos/w9xRFuf2eocFCFVB7",
+			"image": "alumni2015.jpg",
+			"title": "Alumni",
+			"date": "Spring Quarter: May 22, 2015"
+		},
+		{
+			"link": "https://goo.gl/photos/PcctN1qU1kHcp5aG9",
+			"image": "survivor2015.jpg",
+			"title": "Survivor",
+			"date": "Spring Quarter: April 10, 2015"
+		},
+		{
+			"link": "https://goo.gl/photos/CXg267Dq4ybAx4KXA",
+			"image": "liquidfun2015.jpg",
+			"title": "FOOSH vs. Liquid Fun",
+			"date": "Winter Quarter: March 7, 2015"
+		},
+		{
+			"link": "https://goo.gl/photos/YyhbTu2NufUfkbyV7",
+			"image": "improvapocalypse2015.jpg",
+			"title": "Improvapocalypse",
+			"date": "Winter Quarter: January 30-31, 2015"
+		}
+	]		
+};
